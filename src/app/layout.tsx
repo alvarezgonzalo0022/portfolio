d02c8ca692bb7cab8040eeaa5d9263bd3ba597ca
gonzalo-alvarezalvarez_cencosud <gonzalo.alvarezalvarez@cencosud.com.uy>
2024-04-24 12:08:28 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Gonzalo Alvarez',
+    default:
+      'Spencer Sharp - Software developer',
+  },
+  description:
+    'Hi There! I’m Gonzalo, a software developer currently based in Uruguay. I’m a enthusiastic about clean code and good practices. I always look for new challenges and opportunities to learn.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
